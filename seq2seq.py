164a472284981785eb90e5dca03d190597ccd196
lee yong jin <jjeaby@gmail.com>
2018-02-05 20:49:47 +0900

    hangul data add

--- seq2seq.py
+++ seq2seq.py
@@ -1,10 +1,13 @@
 import tensorflow as tf
-
+import matplotlib.pyplot as plt
 
 from tensorflow.python.keras.models import Model
 from tensorflow.python.keras.layers import Input
 from tensorflow.python.keras.layers import LSTM
 from tensorflow.python.keras.layers import Dense
+from tensorflow.python import keras
+from tensorflow.python.keras.callbacks import EarlyStopping
+
 import numpy as np
 
 
@@ -17,10 +20,30 @@ class Seq2Seq:
         self.maxlen = decode_max_len
         self.start_seq_index = start_seq_index
 
+        print("-- decode_max_len --")
+        print(decode_max_len)
+
+        print("-- start_seq_index --")
+        print(start_seq_index)
+
+        print("-- decode_max_len --")
+        print(decode_max_len)
+
+        print("-- input_bits --")
+        print(input_bits)
+
+        print("-- output_bits --")
+        print(output_bits)
+
         # Define an input sequence and process it.
         encoder_inputs = Input(shape=(None, self.input_bits))
+        print(encoder_inputs)
+
         encoder = LSTM(self.encoded_bits, return_state=True)
+        print(encoder)
+
         encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+        print(encoder_outputs)
 
         # We discard `encoder_outputs` and only keep the states.
         encoder_states = [state_h, state_c]
@@ -32,6 +55,8 @@ class Seq2Seq:
         # return states in the training model, but we will use them in inference.
         decoder_lstm = LSTM(self.encoded_bits, return_sequences=True, return_state=True)
         decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+
+        print("dense layer : " + str(self.output_bits))
         decoder_dense = Dense(self.output_bits, activation='softmax')
         decoder_outputs = decoder_dense(decoder_outputs)
 
@@ -53,8 +78,27 @@ class Seq2Seq:
         self.decoder_model = Model([decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states)
 
     def trainModel(self, encoder_input_data, decoder_input_data, decoder_target_data, batch_size=10, epochs=1):
-        self.trainingModel.fit([encoder_input_data, decoder_input_data], decoder_target_data, batch_size=batch_size,
-                               epochs=epochs)
+        tb_hist = keras.callbacks.TensorBoard(log_dir='tblogs', histogram_freq=0, write_graph=True, write_images=True)
+
+
+        hist = self.trainingModel.fit([encoder_input_data, decoder_input_data], decoder_target_data,
+                                      batch_size=batch_size,
+                                      epochs=epochs, callbacks=[tb_hist])
+
+        print(hist.history['loss'])
+        # hist 를 이용하여 loss 그래프 그리기
+        #self.historyLossGraph(hist)
+
+
+    def historyLossGraph(self, hist):
+        loss_ax = plt.subplot()
+        loss_ax.plot(hist.history['loss'], 'y', label='train loss')
+        loss_ax.set_xlabel('epoch')
+        loss_ax.set_ylabel('loss')
+
+        loss_ax.legend(loc='upper left')
+        plt.show()
+
         self.trainingModel.save('s2s.h5')
 
     def loadWeights(self):
